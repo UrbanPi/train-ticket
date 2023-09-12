806fc051af9c941256253cb65a1184fff26f66e2
Pirmin Urbanke <pirmin.urbanke@scch.at>
2023-09-12 16:31:02 +0200

    Make uninstalling more selective

--- hack/deploy/reset.sh
+++ hack/deploy/reset.sh
@@ -8,7 +8,7 @@ namespace="$1"
 
 kubectl delete -f deployment/kubernetes-manifests/quickstart-k8s/yamls -n $namespace
 
-helm ls -n $namespace | grep ts- | awk '{print $1}' | xargs helm uninstall -n $namespace
+helm ls -n $namespace | grep ^ts- | awk '{print $1}' | xargs helm uninstall -n $namespace
 
 helm uninstall $rabbitmqRelease -n $namespace
 helm uninstall $nacosRelease -n $namespace
