65f73418a8218c72cff136b214a2d2482bc1bada
Pirmin Urbanke <pirmin.urbanke@scch.at>
2023-09-26 14:30:53 +0200

    Fix typo

--- script/publish-docker-images.sh
+++ script/publish-docker-images.sh
@@ -24,6 +24,6 @@ docker build --push -t containers.github.scch.at/contest/trainticket/otel-agent:
 --label "org.opencontainers.image.source=https://github.scch.at/ConTest/TrainTicket" \
 --label "org.opencontainers.image.url=https://github.scch.at/ConTest/TrainTicket"
 
-docker build --push -t containers.github.scch.at/contest/trainticket/lotel-collector:latest ./OTEL/otel_collector \
+docker build --push -t containers.github.scch.at/contest/trainticket/otel-collector:latest ./OTEL/otel_collector \
 --label "org.opencontainers.image.source=https://github.scch.at/ConTest/TrainTicket" \
 --label "org.opencontainers.image.url=https://github.scch.at/ConTest/TrainTicket"
