c94554aa312678f658afc9232e6e41889da02f59
Pirmin Urbanke <pirmin.urbanke@scch.at>
2023-10-17 21:09:26 +0200

    Make system executable again: Update java version of docker base image. Add otel agent in deployment. Extract / update deployment files. Add helper scripts and Github Actions. Fix port number of rabbitmq with env variable in deploy yaml. Increase JWT validity.

---
+++ script/publish-docker-images.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eux
+
+echo
+echo "Publishing images, Repo: $1, Tag: error-f4"
+echo
+for dir in ts-*; do
+    if [[ -d $dir ]]; then
+        if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
+            echo "build ${dir}"
+	    # Must use `buildx` as docker build tool
+            docker build --push -t "$1"/"${dir}":error-f4 "$dir"\
+            --label "org.opencontainers.image.source=https://github.scch.at/ConTest/TrainTicket" \
+            --label "org.opencontainers.image.url=https://github.scch.at/ConTest/TrainTicket"
+        fi
+    fi
+done
