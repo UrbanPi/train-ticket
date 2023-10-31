4e3ed2ac30afc4cc4f6a1f6523246453713f91e6
Pirmin Urbanke <pirmin.urbanke@scch.at>
2023-10-31 09:17:46 +0100

    Fix port number, add timeout for request

--- swagger_definitions/get_swagger_definitions.py
+++ swagger_definitions/get_swagger_definitions.py
@@ -1,8 +1,10 @@
 import json
+import socket
 import ssl
 import urllib.request
 from concurrent.futures import ThreadPoolExecutor
 from json import JSONDecodeError
+from urllib.error import URLError
 
 services = [{"name": "ts-admin-basic-info-service", "port": 30030},
             {"name": "ts-admin-order-service", "port": 30031},
@@ -47,7 +49,7 @@ services = [{"name": "ts-admin-basic-info-service", "port": 30030},
             {"name": "ts-user-service", "port": 30070},
             {"name": "ts-verification-code-service", "port": 30071},
             {"name": "ts-voucher-service", "port": 30072},
-            {"name": "ts-food-map-service", "port": 30072},
+            {"name": "ts-food-map-service", "port": 30073},
             {"name": "ts-ticketinfo-service", "port": 30074},
             {"name": "ts-login-service", "port": 30075},
             {"name": "ts-sso-service", "port": 30076},
@@ -115,15 +117,25 @@ def retrieve_and_save(service: dict):
     url = "https://localhost:{}/v2/api-docs".format(service.get("port"))
     print("Getting definition of {} from url: {}".format(service.get("name"), url))
     try:
-        definition: dict = json.loads(urllib.request.urlopen(url, context=ctx).read().decode("utf8"))
+        definition: dict = json.loads(urllib.request.urlopen(url, context=ctx, timeout=30).read().decode("utf8"))
         definition["tags"] = list(filter(lambda tag: not any(tag.get("name") == not_wanted for not_wanted in unwanted_tags), definition.get("tags")))
         definition["paths"] = dict(filter(lambda item: not any(item[0] == not_wanted for not_wanted in unwanted_paths), definition.get("paths").items()))
         with open(service.get("name") + ".json", "w", encoding="utf8") as f:
             f.write(json.dumps(definition))
     except JSONDecodeError | KeyError:
         print("Invalid json for {} from url: {}".format(service.get("name"), url))
+    except URLError as error:
+        if isinstance(error.reason, socket.timeout):
+            print('Timeout Error for {} : This is a request with SSL which is only used in special scenarios of TrainTicket. Maybe you started the wrong file? '.format(url), flush=True)
+        else:
+            print('URL Error: Data of not retrieved because {}\nURL: {}'.format(error, url))
 
 
 if __name__ == '__main__':
     with ThreadPoolExecutor() as executor:
-        executor.map(retrieve_and_save, services)
+        res = executor.map(retrieve_and_save, services)
+
+    executor.shutdown()
+    print("\n".join([str(r) for r in res]))
+
+    input("Finished, press enter to close this window")
