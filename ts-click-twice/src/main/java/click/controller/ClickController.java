d5cf685f311e53c47e2e17440cca3ef317ce7a89
Pirmin Urbanke <pirmin.urbanke@scch.at>
2023-12-04 15:53:13 +0100

    Allow front end to continue ticket booking process after successful order confirmation

--- ts-click-twice/src/main/java/click/controller/ClickController.java
+++ ts-click-twice/src/main/java/click/controller/ClickController.java
@@ -20,12 +20,11 @@ public class ClickController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(path = "/click/clickTwice", method = RequestMethod.POST)
-    public boolean clickTwice(@RequestBody OrderTicketsInfo oti, @CookieValue String loginId, @CookieValue String loginToken) throws Exception{
+    public OrderTicketsResult clickTwice(@RequestBody OrderTicketsInfo oti, @CookieValue String loginId, @CookieValue String loginToken) throws Exception{
         System.out.println("Click Two");
         Future<OrderTicketsResult> task1 = asyncTask.sendAsyncClickTwice(oti,loginId,loginToken);
         Thread.sleep(500);
         Future<OrderTicketsResult> task2 = asyncTask.sendAsyncClickTwice(oti,loginId,loginToken);
-        boolean status = true;
         while(!(task1.isDone() && task2.isDone())){
 
         }
@@ -34,7 +33,7 @@ public class ClickController {
         if(result1.getMessage().contains("ErrorReportUI") || result2.getMessage().contains("ErrorReportUI")){
             throw new Exception("This OrderTicketsResult return before the last loginId request.");
         }else{
-            return status;
+            return result1;
 
         }
     }
