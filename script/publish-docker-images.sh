520a53f1df4b9d015ac9b0cb0abf28fdf76aed44
Pirmin Urbanke <pirmin.urbanke@scch.at>
2023-12-18 16:28:02 +0100

    Fix build script hitting request limit of GitHub server

--- script/publish-docker-images.sh
+++ script/publish-docker-images.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env bash
 set -eux
-
+# Problem: Building and pushing each container sequentially is awfully slow but building and pushing
+# in parallel is too fast (hits the request limit of the GitHub instance).
+# Hence, we build in parallel and wait for each process to finish and then push sequentially.
+tag="ts-error-F22"
 echo
-echo "Publishing images, Repo: $1, Tag: error-f22"
+echo "Publishing images, Repo: $1, Tag: " ${tag}
 echo
+pids=()
 for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             echo "build ${dir}"
 	    # Must use `buildx` as docker build tool
-            docker build --push -t "$1"/"${dir}":error-f22 "$dir"\
+            docker build -t "$1"/"${dir}":"${tag}" "$dir"\
             --label "org.opencontainers.image.source=https://github.scch.at/ConTest/TrainTicket" \
             --label "org.opencontainers.image.url=https://github.scch.at/ConTest/TrainTicket" &
+            pids+=( $! )
         fi
     fi
 done
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait "$pid"
+done
+
+# Push to registry
+for dir in ts-*; do
+    if [[ -d $dir ]]; then
+        if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
+            echo "Pushing ${dir}"
+            docker push "$1"/"${dir}":"${tag}"
+        fi
+    fi
+done
+
