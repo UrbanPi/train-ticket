d700e58b7dcd62b839dce36ec85895b006982611
Pirmin Urbanke <pirmin.urbanke@scch.at>
2023-12-19 11:34:54 +0100

    Automatically use branch name as package tag for building with github action

--- script/publish-docker-images.sh
+++ script/publish-docker-images.sh
@@ -1,17 +1,41 @@
 #!/usr/bin/env bash
 set -eux
-
+# Problem: Building and pushing each container sequentially is awfully slow but building and pushing
+# in parallel is too fast (hits the request limit of the GitHub instance).
+# Hence, we build in parallel and wait for each process to finish and then push sequentially.
+tag=$2
 echo
-echo "Publishing images, Repo: $1, Tag: error-f17"
+echo "Publishing images, Repo: $1, Tag: " "${tag}"
 echo
+pids=()
 for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             echo "build ${dir}"
 	    # Must use `buildx` as docker build tool
-            docker build --push -t "$1"/"${dir}":error-f17 "$dir"\
+            docker build -t "$1"/"${dir}":"${tag}" "$dir"\
             --label "org.opencontainers.image.source=https://github.scch.at/ConTest/TrainTicket" \
             --label "org.opencontainers.image.url=https://github.scch.at/ConTest/TrainTicket" &
+            pids+=( $! )
         fi
     fi
 done
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait "$pid"
+done
+
+# Push to registry (direct docker push is not possible because the image is not stored. However, it should be in the cache.
+# Since nothing changed, the build step should finish fast.
+for dir in ts-*; do
+    if [[ -d $dir ]]; then
+        if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
+            echo "Pushing ${dir}"
+            docker build --push -t "$1"/"${dir}":"${tag}" "$dir"\
+                        --label "org.opencontainers.image.source=https://github.scch.at/ConTest/TrainTicket" \
+                        --label "org.opencontainers.image.url=https://github.scch.at/ConTest/TrainTicket"
+        fi
+    fi
+done
+
