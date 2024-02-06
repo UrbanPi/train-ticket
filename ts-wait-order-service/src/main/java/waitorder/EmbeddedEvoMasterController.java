e9aad0e441824785c5d13bf6b87045c775883358
Pirmin Urbanke <pirmin.urbanke@scch.at>
2024-02-06 20:22:35 +0100

    Add login info. Create SQL connection without Spring

--- ts-wait-order-service/src/main/java/waitorder/EmbeddedEvoMasterController.java
+++ ts-wait-order-service/src/main/java/waitorder/EmbeddedEvoMasterController.java
@@ -3,20 +3,22 @@ package waitorder;
 import org.evomaster.client.java.controller.EmbeddedSutController;
 import org.evomaster.client.java.controller.InstrumentedSutStarter;
 import org.evomaster.client.java.controller.api.dto.AuthenticationDto;
+import org.evomaster.client.java.controller.api.dto.JsonTokenPostLoginDto;
 import org.evomaster.client.java.controller.api.dto.SutInfoDto;
 import org.evomaster.client.java.controller.api.dto.database.schema.DatabaseType;
 import org.evomaster.client.java.controller.db.DbCleaner;
+import org.evomaster.client.java.controller.db.SqlScriptRunner;
 import org.evomaster.client.java.controller.internal.db.DbSpecification;
 import org.evomaster.client.java.controller.problem.ProblemInfo;
 import org.evomaster.client.java.controller.problem.RestProblem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.*;
 
@@ -38,7 +40,7 @@ public class EmbeddedEvoMasterController extends EmbeddedSutController {
     private Connection sqlConnection;
     private String INIT_DB_SCRIPT_PATH = "./complete.sql";
     private List<DbSpecification> dbSpecification = new ArrayList<>();
-
+    private String sqlScript;
     public EmbeddedEvoMasterController() {
         this(40100);
     }
@@ -50,7 +52,6 @@ public class EmbeddedEvoMasterController extends EmbeddedSutController {
 
     @Override
     public String startSut() {
-        String sqlScript;
         try {
             sqlScript = String.join("\n", Files.readAllLines(Paths.get(INIT_DB_SCRIPT_PATH)));
         } catch (IOException e) {
@@ -71,10 +72,18 @@ public class EmbeddedEvoMasterController extends EmbeddedSutController {
                 throw new RuntimeException(e);
             }
         }
-        JdbcTemplate jdbc = ctx.getBean(JdbcTemplate.class);
 
         try {
-            sqlConnection = jdbc.getDataSource().getConnection();
+            sqlConnection = DriverManager.getConnection("jdbc:mysql://ts-db-mysql-leader:3306/ts?useSSL=false", "ts", "Ts_123456");
+//            Alternative to hardcoded SQL connection, get connection from spring context (code below).
+//            Beware, some services do not have a direct connection to the DB but still modify it through other services
+//            (e.g. ts-admin-basic-info-service). These services still need to reset the DB somehow.
+//            When deploying the application with individual databases a different approach is needed e.g. store all
+//            DB connection info here and reset each individually.
+
+//            JdbcTemplate jdbc = ctx.getBean(JdbcTemplate.class);
+//            sqlConnection = jdbc.getDataSource().getConnection();
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -118,6 +127,7 @@ public class EmbeddedEvoMasterController extends EmbeddedSutController {
     @Override
     public void resetStateOfSUT() {
         DbCleaner.clearDatabase(sqlConnection, schemaName, null, DatabaseType.MYSQL);
+        SqlScriptRunner.runScript(sqlConnection, new StringReader(sqlScript));
     }
 
     @Override
@@ -133,9 +143,34 @@ public class EmbeddedEvoMasterController extends EmbeddedSutController {
 
     @Override
     public List<AuthenticationDto> getInfoForAuthentication() {
-        return null;
-    }
+        String loginEndpoint = "http://ts-auth-service:12340/api/v1/users/login";
+        String headerPrefix = "Bearer ";
+        String jwtLocation = "/data/token"; // JSON Pointer
 
+        AuthenticationDto basicUserAuth = new AuthenticationDto("basicUser");
+        JsonTokenPostLoginDto basicUserInfo = new JsonTokenPostLoginDto();
+        basicUserInfo.endpoint = loginEndpoint;
+        basicUserInfo.userId = "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f";
+        basicUserInfo.jsonPayload = "{\"username\":\"fdse_microservice\",\"password\":\"111111\",\"verificationCode\":\"1234\"}";
+        basicUserInfo.headerPrefix = headerPrefix;
+        basicUserInfo.extractTokenField = jwtLocation;
+        basicUserAuth.jsonTokenPostLogin = basicUserInfo;
+
+
+        AuthenticationDto adminUserAuth = new AuthenticationDto("AdminUser");
+        JsonTokenPostLoginDto adminUserInfo = new JsonTokenPostLoginDto();
+        adminUserInfo.endpoint = loginEndpoint;
+        adminUserInfo.userId = "randomUUID";
+        adminUserInfo.jsonPayload = "{\"username\":\"admin\",\"password\":\"222222\"}";
+        adminUserInfo.headerPrefix = headerPrefix;
+        adminUserInfo.extractTokenField = jwtLocation;
+        adminUserAuth.jsonTokenPostLogin = adminUserInfo;
+
+        return Arrays.asList(
+                basicUserAuth,
+                adminUserAuth
+        );
+    }
 
     @Override
     public List<DbSpecification> getDbSpecifications() {
