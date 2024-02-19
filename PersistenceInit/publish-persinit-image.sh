c7295435059d854d83dfa4d1b622c5cdc5909e34
Pirmin Urbanke <pirmin.urbanke@scch.at>
2024-02-19 14:46:13 +0100

    Add preliminary PersistenceInit data

---
+++ PersistenceInit/publish-persinit-image.sh
@@ -0,0 +1,7 @@
+echo
+echo "Publishing PersInit image ts-error-F11"
+echo
+
+docker build --push -t containers.github.scch.at/contest/trainticket/persinit:ts-error-F11 ./PersistenceInit \
+--label "org.opencontainers.image.source=https://github.scch.at/ConTest/TrainTicket" \
+--label "org.opencontainers.image.url=https://github.scch.at/ConTest/TrainTicket"
