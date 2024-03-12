2c7401c5eb6f0146f5193865ffe75c8d6b855232
Pirmin Urbanke <pirmin.urbanke@scch.at>
2024-03-12 12:54:35 +0100

    Use loginToken from cookie instead of JSON payload

--- ts-order-other-service/src/main/java/other/controller/OrderOtherController.java
+++ ts-order-other-service/src/main/java/other/controller/OrderOtherController.java
@@ -88,11 +88,11 @@ public class OrderOtherController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(path = "/orderOther/create", method = RequestMethod.POST)
-    public CreateOrderResult createNewOrder(@RequestBody CreateOrderInfo coi){
+    public CreateOrderResult createNewOrder(@RequestBody CreateOrderInfo coi,@CookieValue String loginToken){
 
         System.out.println("[Order Other Service][Create Order] Create Order form " + coi.getOrder().getFrom() + " --->"
                 + coi.getOrder().getTo() + " at " + coi.getOrder().getTravelDate());
-        VerifyResult tokenResult = verifySsoLogin(coi.getLoginToken());
+        VerifyResult tokenResult = verifySsoLogin(loginToken);
         CreateOrderResult createOrderResult;
         if(tokenResult.isStatus() == true){
             System.out.println("[Order Other Service][Verify Login] Success");
@@ -201,10 +201,10 @@ public class OrderOtherController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(path = "/orderOther/update", method = RequestMethod.POST)
-    public ChangeOrderResult saveOrderInfo(@RequestBody ChangeOrderInfo orderInfo){
+    public ChangeOrderResult saveOrderInfo(@RequestBody ChangeOrderInfo orderInfo,@CookieValue String loginToken){
 
         ChangeOrderResult result;
-        VerifyResult tokenResult = verifySsoLogin(orderInfo.getLoginToken());
+        VerifyResult tokenResult = verifySsoLogin(loginToken);
         if(tokenResult.isStatus() == true){
             System.out.println("[Order Other Service][Verify Login] Success");
             result =  orderService.saveChanges(orderInfo.getOrder());
