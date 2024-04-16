fcf4a9edf617ecc3a86c9a821b648ca65d378de5
Pirmin Urbanke <pirmin.urbanke@scch.at>
2024-04-16 20:59:57 +0200

    Fix table creation statement (deliberate error is in select statement -> wrong column names)

--- ts-voucher-service/server.py
+++ ts-voucher-service/server.py
@@ -142,17 +142,17 @@ def initDatabase():
     #Create the table
     sql = """
     CREATE TABLE if not exists voucherservice.voucher (
-    voucher_id INT NOT NULL AUTO_INCREMENT,       'voucher_id,'
-    order_id VARCHAR(1024) NOT NULL,       'order_id,'
-    travelDate VARCHAR(1024) NOT NULL,       'travelDate,'
-    travelTime VARCHAR(1024) NOT NULL,       
-    contactName VARCHAR(1024) NOT NULL,       'contactName,'
-    trainNumber VARCHAR(1024) NOT NULL,       'train_number'
-    seatClass INT NOT NULL,       
-    seatNumber VARCHAR(1024) NOT NULL,       ',seat_number,'
-    startStation VARCHAR(1024) NOT NULL,       'start_station,'
-    destStation VARCHAR(1024) NOT NULL,       'dest_station,'
-    price FLOAT NOT NULL,'price '
+    voucher_id INT NOT NULL AUTO_INCREMENT,
+    order_id VARCHAR(1024) NOT NULL,
+    travelDate VARCHAR(1024) NOT NULL,
+    travelTime VARCHAR(1024) NOT NULL,
+    contactName VARCHAR(1024) NOT NULL,
+    trainNumber VARCHAR(1024) NOT NULL,
+    seatClass INT NOT NULL,
+    seatNumber VARCHAR(1024) NOT NULL,
+    startStation VARCHAR(1024) NOT NULL,
+    destStation VARCHAR(1024) NOT NULL,
+    price FLOAT NOT NULL,
     PRIMARY KEY (voucher_id));"""
     try:
         cur.execute(sql)
